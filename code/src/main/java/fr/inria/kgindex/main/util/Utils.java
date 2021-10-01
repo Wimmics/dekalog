6d04aa7aca4c1aa4b74305539c7558713116a5e0
Pierre Maillot <maillpierre@gmail.com>
2021-10-01 16:12:51 +0200

    Adding placeholder for stable endpointURL

--- code/src/main/java/fr/inria/kgindex/main/util/Utils.java
+++ code/src/main/java/fr/inria/kgindex/main/util/Utils.java
@@ -25,6 +25,7 @@ public class Utils {
     public static String PLACEHOLDER_FROM = "$FROM";
     public static String PLACEHOLDER_NAMESPACE = "$namespace";
     public static String PLACEHOLDER_ENDPOINTURL = "$endpointUrl";
+    public static String PLACEHOLDER_RAWENDPOINTURL = "$rawEndpointUrl";
 
     /**
      * Replace the placeholders in tests and queries. Placeholders include "$datasetDescription", "$endpointDescription", "$metadataDescription", "$graphList", "$LIMIT", "FROM", "$namespace", "$endpoint", "$name$", $dateLiteral.
@@ -42,6 +43,7 @@ public class Utils {
         queryString = queryString.replaceAll(Pattern.quote(PLACEHOLDER_GRAPHLIST), "<"+describedDataset.getGraphListResource().getURI()+">");
         queryString = queryString.replaceAll(Pattern.quote(PLACEHOLDER_LIMIT), "");
         queryString = queryString.replaceAll(Pattern.quote(PLACEHOLDER_DATASETNAME), describedDataset.getName());
+        queryString = queryString.replaceAll(Pattern.quote(PLACEHOLDER_RAWENDPOINTURL), "<"+describedDataset.getEndpointUrl()+">");
         Date date = new Date();
         Model tmpModel = ModelFactory.createDefaultModel();
         queryString = queryString.replaceAll(Pattern.quote(PLACEHOLDER_DATETIME),"\"" + dateFormatter.format(date) + "\"^^<http://www.w3.org/2001/XMLSchema#datetime>");
