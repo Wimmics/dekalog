3405d4848c4d404bf9a91636bd72d8ee45a91ce9
Pierre Maillot <maillpierre@gmail.com>
2022-08-22 14:56:40 +0200

    namespace fix and README update

--- script/retrieve_LODCloud_catalog/LODCloud_catalog.js
+++ script/retrieve_LODCloud_catalog/LODCloud_catalog.js
@@ -1,6 +1,7 @@
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const $rdf = require('rdflib');
+const fs = require('fs');
 
 const http = require('node:http');
 const https = require('node:https');
@@ -33,10 +34,10 @@ function fetchURLGetPromise(url) {
         keepAlive: true,
         rejectUnauthorized: false
     });
-    
-    let settings = { 
+
+    let settings = {
         method: "GET",
-        agent: function(_parsedURL) {
+        agent: function (_parsedURL) {
             if (_parsedURL.protocol == 'http:') {
                 return httpAgent;
             } else {
@@ -44,7 +45,7 @@ function fetchURLGetPromise(url) {
             }
         }
     };
-    
+
     return fetch(decodeURIComponent(url), settings)
 }
 
@@ -109,6 +110,18 @@ function isURI(value) {
     }
 }
 
+function serializeStorePromise(store) {
+    store.setPrefixForURI("dcat", "http://www.w3.org/ns/dcat#");
+    store.setPrefixForURI("ex", "http://e.g/#");
+    return new Promise((accept, reject) => {
+        $rdf.serialize(null, store, undefined, 'text/turtle', function (err, str) {
+            if (err != null) {
+                reject(err);
+            }
+            accept(str)
+        }, { namespaces: store.namespaces });
+    })
+}
 
 var url = "http://lod-cloud.net/lod-data.json";
 var domain = "http://e.g/#";
@@ -118,79 +131,78 @@ fetchURLGetJSONPromise(url)
         var entryMap = new Map();
         var store = $rdf.graph();
 
-        for([prop, object] of Object.entries(json)) {
+        for ([prop, object] of Object.entries(json)) {
             entryMap.set(prop, object);
 
             var objectUri = domain + encodeURI(prop);
-            console.log(objectUri);
             var datasetResource = $rdf.sym(objectUri);
             store.add(datasetResource, RDF("type"), DCAT("Dataset"));
-            if(object.title != undefined & object.title != null && object.title.length > 0) {
+            if (object.title != undefined & object.title != null && object.title.length > 0) {
                 store.add(datasetResource, DCT("title"), $rdf.lit(object.title));
             }
-            if(object.domain != undefined & object.domain != null && object.domain.length > 0) {
+            if (object.domain != undefined & object.domain != null && object.domain.length > 0) {
                 store.add(datasetResource, SCHEMA("category"), $rdf.lit(object.domain));
             }
-            if(object.license != undefined & object.license != null && object.license.length > 0) {
-                if(isURI(object.license)) {
+            if (object.license != undefined & object.license != null && object.license.length > 0) {
+                if (isURI(object.license)) {
                     store.add(datasetResource, DCT("license"), $rdf.sym(object.license));
                 }
             }
-            if(object.contact_point != undefined & object.contact_point != null ) {
+            if (object.contact_point != undefined & object.contact_point != null) {
                 var contactBN = $rdf.blankNode();
                 store.add(datasetResource, SCHEMA("contactPoint"), contactBN);
                 store.add(contactBN, RDF("type"), SCHEMA("ContactPoint"));
-                if(isLiteral(object.contact_point.email)) {
+                if (isLiteral(object.contact_point.email)) {
                     store.add(contactBN, FOAF("mbox"), $rdf.lit(object.contact_point.email));
-                } else if(isURI(object.contact_point.email)) {
+                } else if (isURI(object.contact_point.email)) {
                     store.add(contactBN, FOAF("mbox"), $rdf.sym(object.contact_point.email));
                 }
-                if(isLiteral(object.contact_point.name)) {
+                if (isLiteral(object.contact_point.name)) {
                     store.add(contactBN, FOAF("name"), $rdf.lit(object.contact_point.name));
                 }
             }
-            if(object.website != undefined & object.website != null && object.website.length > 0) {
-                if(isURI(object.website)) {
+            if (object.website != undefined & object.website != null && object.website.length > 0) {
+                if (isURI(object.website)) {
                     store.add(datasetResource, FOAF("page"), $rdf.sym(object.website))
                 }
             }
-            if(object.description != undefined & object.description != null) {
+            if (object.description != undefined & object.description != null) {
                 Object.entries(object.description).forEach(([lang, description]) => {
-                    if(isLiteral(description)) {
+                    if (isLiteral(description)) {
                         store.add(datasetResource, DCT("description"), $rdf.lit(description, lang));
                     }
                 })
             }
-            if(object.triples != undefined & object.triples != null && object.triples.length > 0) {
-                if(isLiteral(object.triples)) {
+            if (object.triples != undefined & object.triples != null && object.triples.length > 0) {
+                if (isLiteral(object.triples)) {
                     store.add(datasetResource, VOID("triples"), $rdf.lit(object.triples, undefined, XSD("integer")));
                 }
             }
-            if(object.namespace != undefined & object.namespace != null && object.namespace.length > 0) {
-                if(isLiteral(object.namespace)) {
+            if (object.namespace != undefined & object.namespace != null && object.namespace.length > 0) {
+                if (isLiteral(object.namespace)) {
                     store.add(datasetResource, VOID("uriSpace"), $rdf.lit(object.namespace));
                 }
             }
-            if(object.image != undefined & object.image != null && object.image.length > 0) {
-                if(isLiteral(object.image)) {
+            if (object.image != undefined & object.image != null && object.image.length > 0) {
+                if (isLiteral(object.image)) {
                     store.add(datasetResource, FOAF("depiction"), $rdf.lit(object.image));
-                } else if(isURI(object.image)) {
+                } else if (isURI(object.image)) {
                     store.add(datasetResource, FOAF("depiction"), $rdf.sym(object.image));
                 }
             }
-            if(object.keywords != undefined & object.keywords != null && object.keywords.length > 0) {
+            if (object.keywords != undefined & object.keywords != null && object.keywords.length > 0) {
                 object.keywords.forEach(keyword => {
-                    if(isURI(keyword)) {
+                    if (isURI(keyword)) {
                         store.add(datasetResource, DCAT("theme"), $rdf.sym(keyword));
-                    } else if(isLiteral(keyword)) {
+                    } else if (isLiteral(keyword)) {
                         store.add(datasetResource, DCAT("keyword"), $rdf.lit(keyword));
                     }
                 })
             }
 
-            if(object.sparql != undefined & object.sparql != null && object.sparql.length > 0) {
+            if (object.sparql != undefined & object.sparql != null && object.sparql.length > 0) {
                 object.sparql.forEach(sparqlObject => {
-                    if(isURI(sparqlObject.access_url)) {
+                    if (isURI(sparqlObject.access_url)) {
                         var distribBN = $rdf.blankNode();
                         store.add(datasetResource, DCAT("distribution"), distribBN);
                         store.add(distribBN, RDF("type"), DCAT("Distribution"));
@@ -200,39 +212,41 @@ fetchURLGetJSONPromise(url)
                 })
             }
 
-            if(object.full_download != undefined & object.full_download != null && object.full_download.length > 0) {
+            if (object.full_download != undefined & object.full_download != null && object.full_download.length > 0) {
                 object.full_download.forEach(downloadObject => {
                     var distribBN = $rdf.blankNode();
                     store.add(datasetResource, DCAT("distribution"), distribBN);
                     store.add(distribBN, RDF("type"), DCAT("Distribution"));
-                    if(isURI(downloadObject.download_url)) {
+                    if (isURI(downloadObject.download_url)) {
                         store.add(distribBN, DCAT("downloadURL"), $rdf.sym(downloadObject.download_url));
                     }
-                    if(isLiteral(downloadObject.media_type)) {
+                    if (isLiteral(downloadObject.media_type)) {
                         store.add(distribBN, DCT("format"), $rdf.lit(downloadObject.media_type));
                     }
-                    if(isLiteral(downloadObject.title)) {
+                    if (isLiteral(downloadObject.title)) {
                         store.add(distribBN, DCT("title"), $rdf.lit(downloadObject.title));
                     }
-                    if(isLiteral(downloadObject.description)) {
+                    if (isLiteral(downloadObject.description)) {
                         store.add(distribBN, DCT("description"), $rdf.lit(downloadObject.description));
                     }
                 })
             }
         }
 
-        
-        $rdf.serialize(undefined, store, undefined, 'text/turtle', function (err, str) {
-            console.log(str);
-            if (err != null) {
-                console.error(err);
-            }
-        },
-        { namespaces: {
-                dcat:"http://www.w3.org/ns/dcat"
-            }
-        });
+
+        serializeStorePromise(store)
+            .then(str => {
+                const content = str;
+
+                fs.writeFile("catalog.ttl", content, err => {
+                    if (err) {
+                        console.error(err)
+                        return
+                    }
+                    //file written successfully
+                })
+            })
+            .catch(e => {
+                console.error(e);
+            })
     })
-    .catch(e => {
-        console.error(e)
-    }) 
