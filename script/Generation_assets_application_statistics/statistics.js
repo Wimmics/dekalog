18ede776e66d2a5d4f81deb781ef63fa1fb2938a
Pierre Maillot <maillpierre@gmail.com>
2022-08-30 17:25:48 +0200

    working frame

--- script/Generation_assets_application_statistics/statistics.js
+++ script/Generation_assets_application_statistics/statistics.js
@@ -22,33 +22,33 @@ var KGI = $rdf.Namespace("http://ns.inria.fr/kg/index/")
 
 const queryPaginationSize = 500;
 
-function fetchURLGetPromise(url) {
-    const httpAgent = new http.Agent({
-        keepAlive: true,
-        rejectUnauthorized: false
+function fetchPromise(url, header = new Map()) {
+    var myHeaders = new Headers();
+    header.forEach((value, key) => {
+        myHeaders.set(key, value);
     });
-    const httpsAgent = new https.Agent({
-        keepAlive: true,
-        rejectUnauthorized: false
-    });
-
-    let settings = {
-        method: "GET",
-        agent: function (_parsedURL) {
-            if (_parsedURL.protocol == 'http:') {
-                return httpAgent;
-            } else {
-                return httpsAgent;
-            }
-        }
+    var myInit = { 
+        method: 'GET',
+        headers: myHeaders,
+        mode: 'cors',
+        cache: 'no-cache',
+        redirect: 'follow'                 
     };
-
-    return fetch(decodeURIComponent(url), settings)
+    return fetch(url, myInit)
+        .then(response => {
+        if(response.ok) {
+            return response.blob().then(blob => blob.text())
+        } else {
+            throw response;
+        }
+    });
 }
 
 function fetchURLGetJSONPromise(url) {
-    return fetchURLGetPromise(url).then(response => {
-        return response.json();
+    return fetchPromise(url).then(response => {
+        return JSON.parse(response)
+    }).catch(e => {
+        console.error(url, e)
     });
 }
 
@@ -56,11 +56,11 @@ function sparqlQueryPromise(endpoint, query) {
     if (query.includes("SELECT") || query.includes("ASK")) {
         return fetchURLGetJSONPromise(endpoint + '?query=' + encodeURIComponent(query) + '&format=json')
             .catch(error => {
-                console.error(error)
+                console.error(query, error)
             });
     }
     else {
-        console.error(error)
+        console.error(query)
     }
 }
 
@@ -91,12 +91,69 @@ function paginatedSparqlQueryPromise(endpoint, query, limit = queryPaginationSiz
         })
 }
 
+function promisePoolExecution(promiseArray, poolSize = 5, offset = 0) {
+    console.log(offset)
+    var finalResult = [];
+    if ((promiseArray.length > 0) && (offset < promiseArray.length)) {
+        var tmpArray = [];
+        for (var i = 0; i < promiseArray.length && i < poolSize; i++) {
+            tmpArray.push(promiseArray[i]);
+        }
+
+        return Promise.all(tmpArray).then(results => {
+            console.log("Pool n°", offset, "Results: ", results)
+            return promisePoolExecution(promiseArray, poolSize, offset + poolSize).then(nextResults => {
+                finalResult = results.concat(nextResults)
+                return finalResult;
+            });
+        });
+    } else {
+        return new Promise((resolve, reject) => resolve(finalResult));
+    }
+}
 
 // MAIN
-fetch("file:///./queries.json").then(queriesJsonResponse => {
+const dekalogEndpoint = "http://prod-dekalog.inria.fr/sparql";
+fetch("https://raw.githubusercontent.com/Wimmics/dekalog/statscript/script/Generation_assets_application_statistics/queries.json").then(queriesJsonResponse => {
     return queriesJsonResponse.json()
 }).then(queriesJson => {
-    Object.entries(queriesJson).forEach(testFile => {
-        console.log(testFile);
+    var endpointsPromiseArray = []
+    var countResultsEndpointsTestMap = new Map();
+    var countResultsTriplesTestMap = new Map();
+    Object.entries(queriesJson).forEach(testObject => {
+
+        const testFileUrl = testObject[0];
+        const testEndpointsQuery = testObject[1].endpoints;
+        const testTriplesQuery = testObject[1].triples;
+
+        var testEndpointsPromise = sparqlQueryPromise(dekalogEndpoint, testEndpointsQuery).then(results => {
+            var testGraphCountMap = new Map();
+            results.results.bindings.forEach(binding => {
+                var graph = binding.g.value;
+                var count = binding.count.value;
+                testGraphCountMap.set(graph, count);
+            })
+            countResultsEndpointsTestMap.set(testFileUrl, testGraphCountMap);
+            return results;
+        })
+        var testTriplesPromise = sparqlQueryPromise(dekalogEndpoint, testTriplesQuery).then(results => {
+            var testGraphCountMap = new Map();
+            results.results.bindings.forEach(binding => {
+                var graph = binding.g.value;
+                var count = binding.count.value;
+                testGraphCountMap.set(graph, count);
+            })
+            countResultsTriplesTestMap.set(testFileUrl, testGraphCountMap);
+            return results;
+        })
+        endpointsPromiseArray.push(testEndpointsPromise)
+        endpointsPromiseArray.push(testTriplesPromise)
+
     })
+    promisePoolExecution(endpointsPromiseArray).then(results => {
+        console.log(countResultsEndpointsTestMap)
+        console.log(countResultsTriplesTestMap)
+        console.log("END")
+    })
+
 }).catch(error => console.error(error));
